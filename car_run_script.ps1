cd7e52e32a6d8ae558868148d6fa600e21023dfb
Matthew Campbell <mcampbell@qualys.com>
2025-07-18 19:42:28 +0100

    Update to run example

--- car_run_script.ps1
+++ car_run_script.ps1
@@ -1,1 +1,3 @@
 .\car_install_qualys_container_sensor_win.ps1 "dockerhub" "<Activation ID>" "<Customer ID>" "<POD URL Container Security>" "NONE" "install"
+
+.\car_install_qualys_container_sensor_win.ps1 "dockerhub" "f70e5b6a-0cb3-4a24-9994-843c5d40f028" "dc965ac5-375a-dd6a-834d-788f666fad44" "https://gateway.qg1.apps.qualys.co.uk" "NONE" "install"
